1a77b86f42fc8bf4ba8aafd81d4e9ca6edffb809
bakhtbiland <bakhtbiland200@gmail.com>
2023-04-18 22:44:38 +0500

    set the code structure for the project and add some fonts too

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static var primaryColor = Color(0xFFF39A35);
+  static var secondaryColor = Color(0xFFFFB74D);
+  static var backgroundColor = Color(0xFFFFB74D);
+}
---
+++ lib/constants/config.dart
@@ -0,0 +1,11 @@
+class Config {
+  static const testingUrl = "";
+}
+
+class Routes {
+  static const testRoute = "";
+}
+
+class Version {
+  static const appVersion = '1.0.0';
+}
