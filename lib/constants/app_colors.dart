e3f18d6deb0d943f3907fce3f56a6d7334f54d3a
Virajamana <sri@findstoneage.com>
2023-04-24 12:55:44 +0530

    2024/04/24
    main colors submit

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,7 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AppColors {
   static var primaryColor = Color(0xFFF39A35);
   static var secondaryColor = Color(0xFFFFB74D);
-  static var backgroundColor = Color(0xFFFFB74D);
+  static var backgroundColor = Colors. grey[50];
+  static var dangerColor = Color(0xFFA52A2A);
+  static var successColor = Color(0xFF155724);
+
+  static var appTextColorPrimary = Color(0xFF212121);
+  static var appTextColorSecondary = Color(0xFF5A5C5E);
+
+  static var appWhiteColor = Colors.white;
+  static var appBlackColor = Colors.black;
 }
