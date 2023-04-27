2969c9eaf306e1773b28c8d307015faca626473b
bakhtbiland <bakhtbiland200@gmail.com>
2023-04-27 16:09:20 +0500

    using images through object and class

--- lib/models/onboarding_model.dart
+++ lib/models/onboarding_model.dart
@@ -1,3 +1,5 @@
+import 'package:justbuyeight/constants/app_images.dart';
+
 class OnboardingModel {
   final String title;
   final String image;
@@ -11,14 +13,14 @@ class OnboardingModel {
 List<OnboardingModel> contents = [
   OnboardingModel(
     title: "We provide high quality products just for you.",
-    image: "assets/images/onboarding1.jpg",
+    image: ImageAssets.onboarding1,
   ),
   OnboardingModel(
     title: "Your satisfaction is our number one priority.",
-    image: "assets/images/onboarding2.jpg",
+    image: ImageAssets.onboarding2,
   ),
   OnboardingModel(
     title: "Let's fulfill your daily need with JustBuy8 Store right now!",
-    image: "assets/images/onboarding3.jpg",
+    image: ImageAssets.onboarding3,
   ),
 ];
