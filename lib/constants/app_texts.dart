b335bf7e36b05128ec5c6654d74d9f0805a0ad38
bakhtbiland <bakhtbiland200@gmail.com>
2023-04-29 11:36:55 +0500

    declare some new text

---
+++ lib/constants/app_texts.dart
@@ -0,0 +1,9 @@
+class AppText {
+  static const String signInText = "Sign In";
+  static const String forgetPassword = "Forget Password?";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String continueWith = "or continue with";
+  static const String dontHaveAnAccount = "Don't have an account?";
+  static const String signUp = "Signup";
+}
