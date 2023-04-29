2f474c8d5e8ae5f10f938f095178ba9efa9bbc43
bakhtbiland <bakhtbiland200@gmail.com>
2023-04-29 11:36:46 +0500

    create new string

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -4,4 +4,5 @@ class ImageAssets {
   static const String onboarding1 = "assets/images/onboarding1.jpg";
   static const String onboarding2 = "assets/images/onboarding2.jpg";
   static const String onboarding3 = "assets/images/onboarding3.jpg";
+  static const String signInCovert = "assets/images/signin_cover.jpg";
 }
