c17662a22ce31182ccdf5b907f9862cf11c06de4
Wasim <wasimxaman13@gmail.com>
2023-04-29 20:21:04 +0500

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:justbuyeight/screens/authentication/signin_screen.dart';
 import 'package:justbuyeight/screens/authentication/signup_screen.dart';
+import 'package:nb_utils/nb_utils.dart';
 
 void main() {
   runApp(
@@ -23,15 +25,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.height;
+    var width = context.width();
+    var height = context.height();
+
     return ScreenUtilInit(
         designSize: Size(width, height),
         builder: (context, child) {
           return const MaterialApp(
             debugShowCheckedModeBanner: false,
             title: 'JustBuy8',
-            home: SignUpScreen(),
+            home: SignInScreen(),
           );
         });
   }
