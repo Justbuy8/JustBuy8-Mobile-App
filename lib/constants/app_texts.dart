bc58f206a769b3d23f92d26dbf2e6c615837e88d
bakhtbiland <bakhtbiland200@gmail.com>
2023-04-29 15:09:43 +0500

    add new add texts

--- lib/constants/app_texts.dart
+++ lib/constants/app_texts.dart
@@ -2,8 +2,13 @@ class AppText {
   static const String signInText = "Sign In";
   static const String forgetPassword = "Forget Password?";
   static const String email = "Email";
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+
   static const String password = "Password";
   static const String continueWith = "or continue with";
   static const String dontHaveAnAccount = "Don't have an account?";
-  static const String signUp = "Signup";
+  static const String alreadyHaveAnAccount = "Already have an account?";
+
+  static const String signUp = "Sign Up";
 }
