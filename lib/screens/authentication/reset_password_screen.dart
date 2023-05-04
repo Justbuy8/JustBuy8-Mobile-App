3243698cd89c2b8a24200a2f000b2ef793c68695
bakhtbiland <bakhtbiland200@gmail.com>
2023-05-05 00:45:57 +0500

    update the reset password screen

--- lib/screens/authentication/reset_password_screen.dart
+++ lib/screens/authentication/reset_password_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:justbuyeight/constants/app_colors.dart';
 import 'package:justbuyeight/constants/app_texts.dart';
 import 'package:justbuyeight/widgets/components/appbars/basic_appbar_widget.dart';
 import 'package:justbuyeight/widgets/components/buttons/primary_button.dart';
-import 'package:justbuyeight/widgets/components/otp/otp_widget.dart';
 import 'package:justbuyeight/widgets/components/text/secondary_text_widget.dart';
+import 'package:justbuyeight/widgets/components/text_fields/password_text_field_widget.dart';
+import 'package:justbuyeight/widgets/components/text_fields/text_field_widget.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 class ResetPasswordScreen extends StatefulWidget {
@@ -15,49 +18,62 @@ class ResetPasswordScreen extends StatefulWidget {
 }
 
 class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
-  final TextEditingController otpController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
   @override
   void dispose() {
-    otpController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BasicAppbarWidget(
-        title: 'Reset Password',
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20.h,
-              ),
-              const SecondaryTextWidget(
-                  text: "${AppText.resetPasswordText}033000000000"),
-              SizedBox(
-                height: 20.h,
-              ),
-              OtpWidget(
-                controller: otpController,
-                length: 6,
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              PrimaryButtonWidget(
-                  width: context.width(),
-                  height: 50.h,
-                  caption: AppText.sendInstruction,
-                  onPressed: () {}),
-            ],
+        backgroundColor: AppColors.backgroundColor,
+        appBar:
+            const BasicAppbarWidget(title: AppText.resetPasswordAppBarTitle),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20.h,
+                ),
+                const SecondaryTextWidget(
+                  text: AppText.enterNewPassword,
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                PasswordTextFieldWidget(
+                  label: AppText.password,
+                  prefixIcon: Ionicons.lock_closed_outline,
+                  controller: passwordController,
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                PasswordTextFieldWidget(
+                  label: AppText.confirmPassword,
+                  prefixIcon: Ionicons.lock_closed_outline,
+                  controller: confirmPasswordController,
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                PrimaryButtonWidget(
+                    width: context.width(),
+                    height: 50.h,
+                    caption: AppText.changePassword,
+                    onPressed: () {}),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
