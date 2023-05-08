34d1d853a740894b3d23e0eedd644022da24efca
Wasim <wasimxaman13@gmail.com>
2023-05-08 16:58:45 +0500

    main banner widget created

--- lib/constants/app_url.dart
+++ lib/constants/app_url.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings
-
 class AppUrl {
   static const String testBaseUrl = "http://127.0.0.1:8000/api";
   static const String liveBaseUrl = "https://api.goget8.com/api";
-  static const String BaseUrl = liveBaseUrl;
+  static const String baseUrl = liveBaseUrl;
 }
 
 class BannerUrl {
-  static const String getMainBanners = AppUrl.BaseUrl+"/banners/get-main-banners";
+  static const String getMainBanners =
+      "${AppUrl.baseUrl}/banners/get-main-banners";
 }
