21694e227c65a9b2a0ddf3c7f1842970c36759b2
Sri Virajamana <123396657+SriFromFindStoneAge@users.noreply.github.com>
2023-05-09 10:42:05 +0530

    2023/05/09
    SnackBars.dart

---
+++ lib/utils/SnackBars.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: file_names, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:justbuyeight/constants/app_colors.dart';
+
+class SnackBars {
+  static void Danger(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 5),
+        backgroundColor: AppColors.dangerColor,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8), topLeft: Radius.circular(8)))));
+  }
+
+  static void Success(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 5),
+        backgroundColor: AppColors.successColor,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8), topLeft: Radius.circular(8)))));
+  }
+}
