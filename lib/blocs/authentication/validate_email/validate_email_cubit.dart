6b04c10db3ff26dde8d656c19e1aa061d667cfba
bakhtbiland <bakhtbiland200@gmail.com>
2023-05-09 12:33:34 +0500

    validate email code implementation

--- lib/blocs/authentication/validate_email/validate_email_cubit.dart
+++ lib/blocs/authentication/validate_email/validate_email_cubit.dart
@@ -15,31 +15,21 @@ class ValidateEmailCubit extends Cubit<ValidateEmailState> {
   dynamic response;
 
   validateEmail(email) async {
-    // try {
-    response = await AuthenticationController.getEmailVerification(email);
-    //   if (response['Message'] == '' && response['Success'] == true) {
-    //     emit(ValidateEmailSuccessfuly());
-    //   } else if (response['Message'] == 'Email already exist' &&
-    //       response['Success'] == false) {
-    //     emit(ValidateEmailAlreadyExist());
-    //   }
-    // } on SocketException {
-    //   emit(ValidateEmailInternetError());
-    // } on TimeoutException {
-    //   emit(ValidateEmailTimeOut());
-    // } catch (e) {
-    //   emit(ValidateEmailFailed());
-    // }
-
-    //   if (response.statusCode == 200) {
-    //     emit(LoginAuthorizedUser());
-    //   }
-    // } on AuthException catch (e) {
-    //   if (e.message == 'Sign in failed') {
-    //     emit(LoginInternetError());
-    //   } else {
-    //     emit(LoginUnAuthorizedUser());
-    //   }
-    // }
+    try {
+      response = await AuthenticationController.getEmailVerification(email);
+      if (response['Message'] == 'Email is not exist' &&
+          response['Success'] == true) {
+        emit(ValidateEmailSuccessfuly());
+      } else if (response['Message'] == 'Email already exist' &&
+          response['Success'] == false) {
+        emit(ValidateEmailAlreadyExist());
+      }
+    } on SocketException {
+      emit(ValidateEmailInternetError());
+    } on TimeoutException {
+      emit(ValidateEmailTimeOut());
+    } catch (e) {
+      emit(ValidateEmailFailed());
+    }
   }
 }
