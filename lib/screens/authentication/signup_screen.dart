effb4ebc2e5c8c5bb02e842785df3ac18b0865c0
bakhtbiland <bakhtbiland200@gmail.com>
2023-05-09 22:19:30 +0500

    email validation implementation

--- lib/screens/authentication/signup_screen.dart
+++ lib/screens/authentication/signup_screen.dart
@@ -3,9 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:justbuyeight/blocs/authentication/validate_email/validate_email_cubit.dart';
+import 'package:justbuyeight/constants/app_colors.dart';
 import 'package:justbuyeight/constants/app_fonts.dart';
 import 'package:justbuyeight/constants/app_images.dart';
 import 'package:justbuyeight/constants/app_texts.dart';
+import 'package:justbuyeight/constants/bloc_provider.dart';
+import 'package:justbuyeight/screens/authentication/otp_verification_screen.dart';
+import 'package:justbuyeight/utils/SnackBars.dart';
 import 'package:justbuyeight/widgets/components/buttons/primary_button_widget.dart';
 import 'package:justbuyeight/widgets/components/text/primary_text_widget.dart';
 import 'package:justbuyeight/widgets/components/text_fields/password_text_field_widget.dart';
@@ -20,14 +24,27 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final formGlobalKey = GlobalKey<FormState>();
+
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   late ValidateEmailCubit validateEmailCubit;
+  BuildContext? dialogueContext;
 
   initCubit() {
     validateEmailCubit = context.read<ValidateEmailCubit>();
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -41,200 +58,278 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return BlocListener<ValidateEmailCubit, ValidateEmailState>(
       listener: (context, state) {
         if (state is ValidateEmailSuccessfuly) {
-          print("SUCCESSS");
-        } else if (state is ValidateEmailAlreadyExist) {
-          print("ALREADY EXIST");
+          SnackBars.Success(context, "Email verified successfully");
+          Navigator.of(dialogueContext!).pop();
+
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (builder) => const OtpVerificationScreen()));
         } else if (state is ValidateEmailInternetError) {
-          print("Internet Error");
+          SnackBars.Danger(context, "Internet connection failed");
+          Navigator.of(dialogueContext!).pop();
+        } else if (state is ValidateEmailAlreadyExist) {
+          SnackBars.Danger(context, "Email already exist");
+          Navigator.of(dialogueContext!).pop();
         } else if (state is ValidateEmailFailed) {
-          print("Something wrong");
+          SnackBars.Danger(context, "Email verified failed");
+          Navigator.of(dialogueContext!).pop();
         } else if (state is ValidateEmailTimeOut) {
-          print("Timeout");
+          SnackBars.Danger(context, "Timeout");
+          Navigator.of(dialogueContext!).pop();
         }
       },
-      child: Scaffold(
-        body: ListView(
-          children: [
-            SizedBox(
-              height: 180.h,
-              width: context.width(),
-              child: Stack(children: <Widget>[
-                Image.asset(
-                  ImageAssets.signUpCover,
-                  height: 200.h,
-                  width: context.width(),
-                  fit: BoxFit.cover,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 15.w),
+      child: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            return currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          body: ListView(
+            children: [
+              SizedBox(
+                height: 180.h,
+                width: context.width(),
+                child: Stack(children: <Widget>[
+                  Image.asset(
+                    ImageAssets.signUpCover,
+                    height: 200.h,
+                    width: context.width(),
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Ionicons.chevron_back,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          height: 50.h,
+                        ),
+                        PrimaryTextWidget(
+                          text: AppText.signUp,
+                          fontSize: 25.sp,
+                          fontColor: Colors.white,
+                          fontFamily: AppFonts.openSansBold,
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              Form(
+                key: formGlobalKey,
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(
-                        Ionicons.chevron_back,
-                        color: Colors.white,
+                      TextFieldWidget(
+                        controller: _firstNameController,
+                        prefixIcon: Ionicons.person_outline,
+                        label: AppText.firstName,
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Please enter first name";
+                          }
+                          return null;
+                        },
                       ),
                       SizedBox(
-                        height: 50.h,
+                        height: 20.h,
                       ),
-                      PrimaryTextWidget(
-                        text: AppText.signUp,
-                        fontSize: 25.sp,
-                        fontColor: Colors.white,
-                        fontFamily: AppFonts.openSansBold,
+                      TextFieldWidget(
+                        controller: _lastNameController,
+                        prefixIcon: Ionicons.person_outline,
+                        label: AppText.lastName,
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Please enter last name";
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      TextFieldWidget(
+                        controller: _emailController,
+                        prefixIcon: Ionicons.mail_outline,
+                        label: AppText.email,
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Please enter email address";
+                          } else if (RegExp(
+                                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                                  .hasMatch(val) ==
+                              false) {
+                            return "Please enter valid email address";
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      PasswordTextFieldWidget(
+                        controller: _passwordController,
+                        prefixIcon: Ionicons.lock_closed_outline,
+                        label: AppText.password,
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Please enter password";
+                          } else if (val.length < 8) {
+                            return "Password length should be greater than 8";
+                          }
+                          return null;
+                        },
                       ),
                     ],
                   ),
                 ),
-              ]),
-            ),
-            SizedBox(
-              height: 30.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
-              child: Column(
-                children: [
-                  TextFieldWidget(
-                    controller: _firstNameController,
-                    prefixIcon: Ionicons.person_outline,
-                    label: AppText.firstName,
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  TextFieldWidget(
-                    controller: _lastNameController,
-                    prefixIcon: Ionicons.person_outline,
-                    label: AppText.lastName,
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  TextFieldWidget(
-                    controller: _emailController,
-                    prefixIcon: Ionicons.mail_outline,
-                    label: AppText.email,
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  PasswordTextFieldWidget(
-                    controller: _passwordController,
-                    prefixIcon: Ionicons.lock_closed_outline,
-                    label: AppText.password,
-                  ),
-                ],
               ),
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
-              child: PrimaryButtonWidget(
-                  width: context.width(),
-                  height: 50.h,
-                  caption: AppText.signUp,
-                  onPressed: () async {
-                    await validateEmailCubit
-                        .validateEmail(_emailController.text.trim());
-                  }),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
-              child: Row(children: <Widget>[
-                const Expanded(
-                    child: Divider(
-                  color: Colors.black,
-                )),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.w, right: 10.w),
-                  child: PrimaryTextWidget(
-                    text: AppText.continueWith,
-                    fontSize: 14.sp,
-                    fontColor: Colors.black,
-                    fontFamily: AppFonts.openSansLight,
+              SizedBox(
+                height: 10.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
+                child: PrimaryButtonWidget(
+                    width: context.width(),
+                    height: 50.h,
+                    caption: AppText.signUp,
+                    onPressed: () async {
+                      if (formGlobalKey.currentState!.validate()) {
+                        showDialog(
+                            barrierDismissible: false,
+                            context: context,
+                            builder: (_ctx) {
+                              dialogueContext = _ctx;
+                              return Dialog(
+                                backgroundColor: Colors.white,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 20),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      CircularProgressIndicator(
+                                        color: AppColors.primaryColor,
+                                      ),
+                                      const SizedBox(
+                                        height: 15,
+                                      ),
+                                      const Text('Loading...')
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                        await validateEmailCubit
+                            .validateEmail(_emailController.text.trim());
+                      }
+                    }),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
+                child: Row(children: <Widget>[
+                  const Expanded(
+                      child: Divider(
+                    color: Colors.black,
+                  )),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.w, right: 10.w),
+                    child: PrimaryTextWidget(
+                      text: AppText.continueWith,
+                      fontSize: 14.sp,
+                      fontColor: Colors.black,
+                      fontFamily: AppFonts.openSansLight,
+                    ),
                   ),
-                ),
-                const Expanded(
-                    child: Divider(
-                  color: Colors.black,
-                )),
-              ]),
-            ),
-            SizedBox(height: 20.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(8.w),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8.r)),
-                  child: const Icon(
-                    Ionicons.logo_facebook,
-                  ),
-                ),
-                SizedBox(
-                  width: 10.w,
-                ),
-                Container(
-                  padding: EdgeInsets.all(8.w),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8.r)),
-                  child: const Icon(
-                    Ionicons.logo_google,
-                  ),
-                ),
-                SizedBox(
-                  width: 10.w,
-                ),
-                Container(
-                  padding: EdgeInsets.all(8.w),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8.r)),
-                  child: const Icon(
-                    Ionicons.logo_apple,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.w, right: 10.w),
-              child: Row(
+                  const Expanded(
+                      child: Divider(
+                    color: Colors.black,
+                  )),
+                ]),
+              ),
+              SizedBox(height: 20.h),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  PrimaryTextWidget(
-                    text: AppText.alreadyHaveAnAccount,
-                    fontSize: 14.sp,
-                    fontFamily: AppFonts.openSansLight,
+                  Container(
+                    padding: EdgeInsets.all(8.w),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8.r)),
+                    child: const Icon(
+                      Ionicons.logo_facebook,
+                    ),
                   ),
                   SizedBox(
-                    width: 5.w,
+                    width: 10.w,
                   ),
-                  PrimaryTextWidget(
-                    text: AppText.signInText,
-                    fontSize: 14.sp,
-                    fontColor: Colors.black,
-                    fontFamily: AppFonts.robotoBold,
+                  Container(
+                    padding: EdgeInsets.all(8.w),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8.r)),
+                    child: const Icon(
+                      Ionicons.logo_google,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8.w),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8.r)),
+                    child: const Icon(
+                      Ionicons.logo_apple,
+                    ),
                   ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-          ],
+              SizedBox(
+                height: 20.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.w, right: 10.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    PrimaryTextWidget(
+                      text: AppText.alreadyHaveAnAccount,
+                      fontSize: 14.sp,
+                      fontFamily: AppFonts.openSansLight,
+                    ),
+                    SizedBox(
+                      width: 5.w,
+                    ),
+                    PrimaryTextWidget(
+                      text: AppText.signInText,
+                      fontSize: 14.sp,
+                      fontColor: Colors.black,
+                      fontFamily: AppFonts.robotoBold,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/components/text_fields/text_field_widget.dart
+++ lib/widgets/components/text_fields/text_field_widget.dart
@@ -30,34 +30,40 @@ class TextFieldWidget extends StatefulWidget {
 class _TextFieldWidgetState extends State<TextFieldWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: context.height() / 15,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(10)),
-        color: AppColors.appGreyColor,
-      ),
-      alignment: Alignment.center,
-      child: TextFormField(
-        controller: widget.controller,
-        style: const TextStyle(fontSize: 18),
-        //focusNode: widget.focusNode ?? FocusNode(),
-        //onFieldSubmitted: (value) => widget.focusNode?.nextFocus(),
-        textInputAction: widget.textInputAction ?? TextInputAction.done,
-        keyboardType: widget.keyboardType ?? TextInputType.text,
-        validator: widget.validator ?? (value) => null,
-        decoration: InputDecoration(
-          hintText: widget.label ?? "",
-          prefixIcon: Icon(
-            widget.prefixIcon,
-            size: 28,
-            color: AppColors.primaryColor,
-          ),
-          suffixIcon: Icon(
-            widget.suffixIcon,
-            size: 28,
-          ),
-          border: InputBorder.none,
+    return TextFormField(
+      controller: widget.controller,
+      style: const TextStyle(fontSize: 18),
+      //focusNode: widget.focusNode ?? FocusNode(),
+      //onFieldSubmitted: (value) => widget.focusNode?.nextFocus(),
+      textInputAction: widget.textInputAction ?? TextInputAction.done,
+      keyboardType: widget.keyboardType ?? TextInputType.text,
+      validator: widget.validator ?? (value) => null,
+      decoration: InputDecoration(
+        errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none),
+        focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none),
+        fillColor: AppColors.appGreyColor,
+        filled: true,
+        hintText: widget.label ?? "",
+        prefixIcon: Icon(
+          widget.prefixIcon,
+          size: 28,
+          color: AppColors.primaryColor,
         ),
+        suffixIcon: Icon(
+          widget.suffixIcon,
+          size: 28,
+        ),
+        border: InputBorder.none,
       ),
     );
   }
