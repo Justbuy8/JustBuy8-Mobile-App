361b33aaf0e0edc305e9c815de96dfb1fbed7bb9
Wasim <wasimxaman13@gmail.com>
2023-05-11 11:45:40 +0500

    favorite icon added to featured product widget

--- lib/screens/maintabs/home/widgets/products/featured_products_widget.dart
+++ lib/screens/maintabs/home/widgets/products/featured_products_widget.dart
@@ -1,5 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:justbuyeight/constants/app_colors.dart';
 import 'package:justbuyeight/constants/app_fonts.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -16,20 +18,39 @@ class FeaturedProductWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          height: context.height() * 0.2,
-          width: context.width() * 0.4,
-          padding: const EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: CachedNetworkImage(
-            imageUrl: imageUrl,
-            fit: BoxFit.contain,
-            height: context.height() * 0.2,
-            width: context.width() * 0.4,
-          ),
+        Stack(
+          children: [
+            Container(
+              height: context.height() * 0.2,
+              width: context.width() * 0.4,
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: AppColors.appGreyColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: CachedNetworkImage(
+                imageUrl: imageUrl,
+                fit: BoxFit.contain,
+                height: context.height() * 0.2,
+                width: context.width() * 0.4,
+              ),
+            ),
+            Positioned(
+              right: 0,
+              top: 0,
+              child: IconButton(
+                onPressed: () {},
+                icon: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: AppColors.appGreyColor,
+                  child: Icon(
+                    Ionicons.heart_outline,
+                    color: AppColors.appBlackColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
         const SizedBox(height: 10),
         SizedBox(
