d0dbe230d9216a2a9103f94b5410304dc0abb745
Virajamana <sri@findstoneage.com>
2023-05-21 02:03:23 +0530

    2023/05/21
    Fixed some issues

--- lib/screens/maintabs/home/home_screen.dart
+++ lib/screens/maintabs/home/home_screen.dart
@@ -8,11 +8,13 @@ import 'package:justbuyeight/constants/app_texts.dart';
 import 'package:justbuyeight/constants/bloc_provider.dart';
 import 'package:justbuyeight/constants/secure_storage.dart';
 import 'package:justbuyeight/screens/authentication/signin_screen.dart';
+import 'package:justbuyeight/screens/brands/choose_brands_screen.dart';
 import 'package:justbuyeight/screens/maintabs/home/widgets/products/featured/featured_products_listview.dart';
 import 'package:justbuyeight/screens/maintabs/home/widgets/products/new_arrival/new_arrival_grid.dart';
 import 'package:justbuyeight/screens/maintabs/home/widgets/products/top/top_categories_widgets.dart';
 import 'package:justbuyeight/screens/maintabs/home/widgets/title-button/title_and_button_widget.dart';
 import 'package:justbuyeight/utils/AlertDialog.dart';
+import 'package:justbuyeight/utils/Navigator.dart';
 import 'package:justbuyeight/widgets/components/appbars/secondary_appbar_widget.dart';
 import 'package:justbuyeight/widgets/components/banners/FooterBannerWidget.dart';
 import 'package:justbuyeight/widgets/components/banners/MainBannerWidget.dart';
@@ -85,6 +87,7 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+
             /// footer banner
             FooterBannerWidget(),
             Padding(
@@ -94,13 +97,17 @@ class HomeScreen extends StatelessWidget {
                   /// choose brands
                   TitleAndButtonWidget(
                     text: AppText.chooseBrandsText,
-                    onPressed: () {},
+                    onPressed: () => TabNavigator.withoutTabNavigator(
+                        context: context, screen: ChooseBrandsScreen()),
                   ),
                   SizedBox(height: 20),
                   BrandsWidget(),
                   SizedBox(height: 20),
+
                   /// new arrivals
-                  TitleAndButtonWidget(text: AppText.newArrivalsText),
+                  TitleAndButtonWidget(
+                    text: AppText.newArrivalsText,
+                  ),
                   SizedBox(height: 20),
                   CategoriesWidget(),
                   SizedBox(height: 20),
