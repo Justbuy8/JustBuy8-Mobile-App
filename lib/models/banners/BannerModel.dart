1cc9b783d65268bfab30add88f0609ba73ec517a
Wasim <wasimxaman13@gmail.com>
2023-05-20 15:23:06 +0500

    Update BannerModel.dart

--- lib/models/banners/BannerModel.dart
+++ lib/models/banners/BannerModel.dart
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // ignore_for_file: file_names
 
 import 'dart:convert';
@@ -37,3 +38,46 @@ class BannerModel {
         "Resource id": resourceId,
     };
 }
+=======
+// ignore_for_file: file_names
+
+import 'dart:convert';
+
+List<BannerModel> bannerModelFromJson(String str) => List<BannerModel>.from(
+    json.decode(str).map((x) => BannerModel.fromJson(x)));
+
+String bannerModelToJson(List<BannerModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class BannerModel {
+  int id;
+  String imageLink;
+  String url;
+  String resourceType;
+  String resourceId;
+
+  BannerModel({
+    required this.id,
+    required this.imageLink,
+    required this.url,
+    required this.resourceType,
+    required this.resourceId,
+  });
+
+  factory BannerModel.fromJson(Map<String, dynamic> json) => BannerModel(
+        id: json["Id"],
+        imageLink: json["Image Link"],
+        url: json["Url"],
+        resourceType: json["Resource Type"],
+        resourceId: json["Resource id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "Id": id,
+        "Image Link": imageLink,
+        "Url": url,
+        "Resource Type": resourceType,
+        "Resource id": resourceId,
+      };
+}
+>>>>>>> Stashed changes
