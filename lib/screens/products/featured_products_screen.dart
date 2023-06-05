4287f11bbe92f585306bf1ec59cae0f08f99f3a8
Wasim <wasimxaman13@gmail.com>
2023-06-05 10:05:14 +0500

    done

--- lib/screens/products/featured_products_screen.dart
+++ lib/screens/products/featured_products_screen.dart
@@ -7,7 +7,7 @@ import 'package:justbuyeight/constants/app_texts.dart';
 import 'package:justbuyeight/models/products/ProductModel.dart';
 import 'package:justbuyeight/screens/maintabs/home/widgets/products/product_widget.dart';
 import 'package:justbuyeight/widgets/components/appbars/basic_appbar_widget.dart';
-import 'package:nb_utils/nb_utils.dart';
+import 'package:justbuyeight/widgets/components/shimmer/rectangular_shimmer.dart';
 
 class FeaturedProductsScreen extends StatefulWidget {
   const FeaturedProductsScreen({Key? key}) : super(key: key);
@@ -51,32 +51,47 @@ class _FeaturedProductsScreenState extends State<FeaturedProductsScreen> {
     return Scaffold(
       appBar: const BasicAppbarWidget(title: AppText.featuredProductsText),
       body: SafeArea(
-          child: BlocConsumer<FeaturedProductsBloc, FeaturedProductsState>(
-        bloc: bloc,
-        listener: (context, state) {
-          if (state is FeaturedProductsGetState) {
-            products.addAll(state.products);
-          }
-        },
-        builder: (context, state) {
-          return products.isEmpty
-              ? NoDataWidget()
-              : GridView.builder(
-                  controller: _scrollController,
-                  itemCount: products.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.7,
-                    mainAxisSpacing: 20,
-                    crossAxisSpacing: 20,
-                  ),
-                  itemBuilder: (context, index) {
-                    return ProductWidget(product: products[index]);
-                  },
-                  padding: const EdgeInsets.all(10.0),
-                );
-        },
-      )),
+        child: BlocConsumer<FeaturedProductsBloc, FeaturedProductsState>(
+          bloc: bloc,
+          listener: (context, state) {
+            if (state is FeaturedProductsGetState) {
+              products.addAll(state.products);
+            }
+          },
+          builder: (context, state) {
+            return products.isEmpty
+                ? GridView.builder(
+                    itemCount: 10,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                    ),
+                    itemBuilder: (context, index) {
+                      return const RectangularShimmer();
+                    },
+                    padding: const EdgeInsets.all(10.0),
+                  )
+                : GridView.builder(
+                    controller: _scrollController,
+                    itemCount: products.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                    ),
+                    itemBuilder: (context, index) {
+                      return ProductWidget(product: products[index]);
+                    },
+                    padding: const EdgeInsets.all(10.0),
+                  );
+          },
+        ),
+      ),
     );
   }
 }
