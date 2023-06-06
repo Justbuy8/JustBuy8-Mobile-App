9499b7df5d299d59439004728bbc2d1cbb81248d
Wasim <wasimxaman13@gmail.com>
2023-06-06 09:31:14 +0500

    Update main_tabs_screen.dart

--- lib/screens/maintabs/main_tabs_screen.dart
+++ lib/screens/maintabs/main_tabs_screen.dart
@@ -26,15 +26,9 @@ class _MainTabsScreenState extends State<MainTabsScreen> {
   int tabindex = 0;
   static List<Widget> homepageTabs = <Widget>[
     HomeScreen(),
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.blue,
-    ),
-    Container(
-      color: Colors.yellow,
-    ),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+    Container(color: Colors.yellow),
     MyAccountScreen(),
   ];
 
@@ -46,55 +40,57 @@ class _MainTabsScreenState extends State<MainTabsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          AnimatedSwitcher(
-            transitionBuilder: (Widget child, Animation<double> animation) {
-              return ScaleTransition(scale: animation, child: child);
-            },
-            duration: const Duration(milliseconds: 500),
-            child: FadeIndexedStack(
-              index: tabindex,
-              children: homepageTabs,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            AnimatedSwitcher(
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(scale: animation, child: child);
+              },
+              duration: const Duration(milliseconds: 500),
+              child: FadeIndexedStack(
+                index: tabindex,
+                children: homepageTabs,
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
-              alignment: Alignment.center,
-              height: Platform.isAndroid ? 55 : 60,
-              decoration: BoxDecoration(
-                  color: AppColors.primaryColor,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              child: HomeBottomNavBar(context, onTabChange: (index) {
-                if (tabindex != index) {
-                  setState(() {
-                    tabindex = index;
-                  });
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                alignment: Alignment.center,
+                height: Platform.isAndroid ? 55 : 60,
+                decoration: BoxDecoration(
+                    color: AppColors.primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: HomeBottomNavBar(context, onTabChange: (index) {
+                  if (tabindex != index) {
+                    setState(() {
+                      tabindex = index;
+                    });
 
-                  //Home Page
-                  if (tabindex == 0) {}
+                    //Home Page
+                    if (tabindex == 0) {}
 
-                  //Search Page
-                  if (tabindex == 1) {}
+                    //Search Page
+                    if (tabindex == 1) {}
 
-                  //Cart Page
-                  if (tabindex == 2) {}
+                    //Cart Page
+                    if (tabindex == 2) {}
 
-                  //WishList Page
-                  if (tabindex == 3) {}
+                    //WishList Page
+                    if (tabindex == 3) {}
 
-                  //Account Page
-                  if (index == 4) {
-                    context.read<MyaccountCubit>().myAccount();
+                    //Account Page
+                    if (index == 4) {
+                      context.read<MyaccountCubit>().myAccount();
+                    }
                   }
-                }
-              }),
-            ),
-          )
-        ],
+                }),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
