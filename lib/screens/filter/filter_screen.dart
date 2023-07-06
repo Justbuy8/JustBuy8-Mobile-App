9ba44bf7550ebf815f8394d9dd0db275f44b5913
Wasim <wasimxaman13@gmail.com>
2023-07-06 17:05:42 +0500

    filter screen ui completed

--- lib/screens/filter/filter_screen.dart
+++ lib/screens/filter/filter_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:justbuyeight/constants/app_texts.dart';
+import 'package:justbuyeight/screens/filter/widgets/rating_filter_widget.dart';
 import 'package:justbuyeight/screens/filter/widgets/toggle_buttons_widget.dart';
 import 'package:justbuyeight/widgets/components/appbars/basic_appbar_widget.dart';
-import 'package:justbuyeight/widgets/components/text/primary_text_widget.dart';
+import 'package:justbuyeight/widgets/components/buttons/primary_button.dart';
+import 'package:justbuyeight/widgets/components/category/filter_categories_widget.dart';
 
 class FilterScreen extends StatefulWidget {
   const FilterScreen({Key? key}) : super(key: key);
@@ -33,35 +35,59 @@ class _FilterScreenState extends State<FilterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const BasicAppbarWidget(title: AppText.filterText),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ToggleButtonsWidget(searchTypes: searchTypes),
-                const SizedBox(height: 20),
-                const PrimaryTextWidget(text: "Price"),
-                RangeSlider(
-                  values: _currentRangeValues,
-                  onChanged: (RangeValues values) {
-                    setState(() {
-                      _currentRangeValues = values;
-                    });
-                  },
-                  min: 1,
-                  max: 200,
-                  divisions: 200,
-                  labels: RangeLabels(
-                    "€${_currentRangeValues.start.round()}",
-                    "€${_currentRangeValues.end.round()}",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ToggleButtonsWidget(searchTypes: searchTypes),
+                  const SizedBox(height: 20),
+                  Text(
+                    AppText.categoriesText,
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  const FilterCategoriesWidget(),
+                  const SizedBox(height: 20),
+                  Text(
+                    AppText.priceText,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  RangeSlider(
+                    values: _currentRangeValues,
+                    onChanged: (RangeValues values) {
+                      setState(() {
+                        _currentRangeValues = values;
+                      });
+                    },
+                    min: 1,
+                    max: 200,
+                    divisions: 200,
+                    labels: RangeLabels(
+                      "€${_currentRangeValues.start.round()}",
+                      "€${_currentRangeValues.end.round()}",
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    AppText.ratingText,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 20),
+                  const RatingFilterWidget(),
+                  const SizedBox(height: 20),
+                  PrimaryButtonWidget(
+                    caption: AppText.filterText,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
