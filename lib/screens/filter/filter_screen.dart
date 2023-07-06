e8905b90b7a4099b91e79015e6270d25e623ff07
Wasim <wasimxaman13@gmail.com>
2023-07-06 20:20:57 +0500

    filter screen UI completed

--- lib/screens/filter/filter_screen.dart
+++ lib/screens/filter/filter_screen.dart
@@ -3,7 +3,7 @@ import 'package:justbuyeight/constants/app_texts.dart';
 import 'package:justbuyeight/screens/filter/widgets/rating_filter_widget.dart';
 import 'package:justbuyeight/screens/filter/widgets/toggle_buttons_widget.dart';
 import 'package:justbuyeight/widgets/components/appbars/basic_appbar_widget.dart';
-import 'package:justbuyeight/widgets/components/buttons/primary_button.dart';
+import 'package:justbuyeight/widgets/components/buttons/primary_button_widget.dart';
 import 'package:justbuyeight/widgets/components/category/filter_categories_widget.dart';
 
 class FilterScreen extends StatefulWidget {
@@ -35,56 +35,60 @@ class _FilterScreenState extends State<FilterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const BasicAppbarWidget(title: AppText.filterText),
-      body: SingleChildScrollView(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
+            Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ToggleButtonsWidget(searchTypes: searchTypes),
-                  const SizedBox(height: 20),
-                  Text(
-                    AppText.categoriesText,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(height: 20),
-                  const FilterCategoriesWidget(),
-                  const SizedBox(height: 20),
-                  Text(
-                    AppText.priceText,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  RangeSlider(
-                    values: _currentRangeValues,
-                    onChanged: (RangeValues values) {
-                      setState(() {
-                        _currentRangeValues = values;
-                      });
-                    },
-                    min: 1,
-                    max: 200,
-                    divisions: 200,
-                    labels: RangeLabels(
-                      "€${_currentRangeValues.start.round()}",
-                      "€${_currentRangeValues.end.round()}",
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    AppText.ratingText,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(height: 20),
-                  const RatingFilterWidget(),
-                  const SizedBox(height: 20),
-                  PrimaryButtonWidget(
-                    caption: AppText.filterText,
-                    onPressed: () {},
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ToggleButtonsWidget(searchTypes: searchTypes),
+                      const SizedBox(height: 20),
+                      Text(
+                        AppText.categoriesText,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 20),
+                      const FilterCategoriesWidget(),
+                      const SizedBox(height: 20),
+                      Text(
+                        AppText.priceText,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      RangeSlider(
+                        values: _currentRangeValues,
+                        onChanged: (RangeValues values) {
+                          setState(() {
+                            _currentRangeValues = values;
+                          });
+                        },
+                        min: 1,
+                        max: 200,
+                        divisions: 200,
+                        labels: RangeLabels(
+                          "€${_currentRangeValues.start.round()}",
+                          "€${_currentRangeValues.end.round()}",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        AppText.ratingText,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 20),
+                      const RatingFilterWidget(),
+                      const SizedBox(height: 20),
+                    ],
                   ),
                 ],
               ),
+            ),
+            PrimaryButtonWidget(
+              caption: AppText.filterText,
+              onPressed: () {},
             ),
           ],
         ),
