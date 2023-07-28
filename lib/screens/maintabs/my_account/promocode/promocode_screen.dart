68d2f91ad603b77a7cf663cb9db347b310675a14
bakhtbiland <bakhtbiland200@gmail.com>
2023-07-28 16:56:55 +0500

    update the bottom sheet

--- lib/screens/maintabs/my_account/promocode/promocode_screen.dart
+++ lib/screens/maintabs/my_account/promocode/promocode_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:justbuyeight/constants/app_colors.dart';
 import 'package:justbuyeight/constants/app_fonts.dart';
 import 'package:justbuyeight/constants/app_images.dart';
 import 'package:justbuyeight/constants/app_texts.dart';
@@ -26,61 +27,121 @@ class _PromodeCodeScreenState extends State<PromodeCodeScreen> {
     return Scaffold(
       appBar: BasicAppbarWidget(title: 'My Promocodes'),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20.h,
-              ),
-              Image.asset(
-                ImageAssets.promocodeImage,
-                color: Color.fromARGB(255, 146, 46, 46),
-              ),
-              SizedBox(
-                height: 30.h,
-              ),
-              PrimaryTextWidget(
-                text: AppText.dontHaveText,
-                fontSize: 28,
-                fontFamily: AppFonts.openSansBold,
-                textAlign: TextAlign.center,
-              ),
-              PrimaryTextWidget(
-                text: AppText.promocodeText,
-                fontSize: 28,
-                fontFamily: AppFonts.openSansBold,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              const SecondaryTextWidget(text: AppText.promocodeSubTitleText),
-              SizedBox(
-                height: 30.h,
-              ),
-              TextFieldWidget(
-                controller: _couponCodeController,
-                prefixIcon: Icons.card_giftcard_outlined,
-                label: 'Enter Your Coupon Code',
-              ),
-              SizedBox(
-                height: 30.h,
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: PrimaryButtonWidget(
-                    width: context.width(),
-                    height: 50.h,
-                    caption: AppText.promoCode,
-                    onPressed: () {
-                      // Navigator.of(context).push(MaterialPageRoute(
-                      //     builder: (builder) => MainTabsScreen()));
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 15.w, right: 15.w, bottom: 20.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ///REUSED AGAIN///
+
+                SizedBox(
+                  height: 20.h,
+                ),
+                // Image.asset(
+                //   ImageAssets.promocodeImage,
+                //   color: Color.fromARGB(255, 146, 46, 46),
+                // ),
+                // SizedBox(
+                //   height: 30.h,
+                // ),
+                // PrimaryTextWidget(
+                //   text: AppText.dontHaveText,
+                //   fontSize: 28,
+                //   fontFamily: AppFonts.openSansBold,
+                //   textAlign: TextAlign.center,
+                // ),
+                // PrimaryTextWidget(
+                //   text: AppText.promocodeText,
+                //   fontSize: 28,
+                //   fontFamily: AppFonts.openSansBold,
+                //   textAlign: TextAlign.center,
+                // ),
+                // SizedBox(
+                //   height: 20.h,
+                // ),
+                // const SecondaryTextWidget(text: AppText.promocodeSubTitleText),
+                // SizedBox(
+                //   height: 30.h,
+                // ),
+                TextFieldWidget(
+                  controller: _couponCodeController,
+                  prefixIcon: Icons.card_giftcard_outlined,
+                  label: 'Enter Your Coupon Code',
+                ),
+                SizedBox(
+                  height: 30.h,
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: PrimaryButtonWidget(
+                      width: context.width(),
+                      height: 50.h,
+                      caption: AppText.promoCode,
+                      onPressed: () {
+                        // Navigator.of(context).push(MaterialPageRoute(
+                        //     builder: (builder) => MainTabsScreen()));
+                      }),
+                ),
+                SizedBox(
+                  height: 15.h,
+                ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.only(bottom: 10.w),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: AppColors.appGreyColor)),
+                          child: Padding(
+                            padding: EdgeInsets.all(15.w),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 10.h,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Icon(Icons.card_giftcard_outlined),
+                                        SizedBox(
+                                          width: 10.w,
+                                        ),
+                                        PrimaryTextWidget(text: '20%'),
+                                      ],
+                                    ),
+                                    Icon(Icons.copy),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 20.h,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    SecondaryTextWidget(
+                                        text: 'Expires Sep 20,2023'),
+                                    SecondaryTextWidget(text: '20firstorder')
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
                     }),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
