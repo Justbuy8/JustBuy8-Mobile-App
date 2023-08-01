1bf8c0be5fbc43a3b581b544ce57c0c919682ce1
bakhtbiland <bakhtbiland200@gmail.com>
2023-08-01 16:04:00 +0500

    update the cart screen

--- lib/screens/maintabs/cart/cart_screen.dart
+++ lib/screens/maintabs/cart/cart_screen.dart
@@ -29,238 +29,234 @@ class _CartScreenState extends State<CartScreen> {
         leadingIcon: Ionicons.settings_outline,
         trailingIcon: Ionicons.notifications_outline,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(10.w),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 10.h,
+      body: Padding(
+        padding: EdgeInsets.all(10.w),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 10.h,
+            ),
+            // Center(
+            //   child: Image.asset(
+            //     ImageAssets.emptyCart,
+            //     height: context.height() * 0.3,
+            //   ),
+            // ),
+            // 20.height,
+            // Text(
+            //   AppText.yourCartIsEmptyText,
+            //   style: AppTextStyle.cartTextStyle,
+            // ),
+            // 10.height,
+            // const Text(AppText.looksLikeYouHaventMadeYourOrderYetText),
+            // 40.height,
+            // GestureDetector(
+            //   onTap: () {},
+            //   child: Container(
+            //     padding:
+            //         const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            //     width: double.infinity,
+            //     alignment: Alignment.center,
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(10),
+            //       border: Border.all(color: AppColors.primaryColor),
+            //     ),
+            //     child: Text(
+            //       AppText.shopNowText,
+            //       style: TextStyle(
+            //         color: AppColors.primaryColor,
+            //         fontSize: 20,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            ListView.builder(
+                itemCount: 2,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      CartCardWidget(),
+                      Divider(),
+                    ],
+                  );
+                }),
+            SizedBox(
+              height: 20.h,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10.w, right: 20.w),
+              child: PrimaryTextWidget(
+                text: 'Promo Code',
+                fontSize: 15,
+                fontFamily: AppFonts.robotoBold,
               ),
-              // Center(
-              //   child: Image.asset(
-              //     ImageAssets.emptyCart,
-              //     height: context.height() * 0.3,
-              //   ),
-              // ),
-              // 20.height,
-              // Text(
-              //   AppText.yourCartIsEmptyText,
-              //   style: AppTextStyle.cartTextStyle,
-              // ),
-              // 10.height,
-              // const Text(AppText.looksLikeYouHaventMadeYourOrderYetText),
-              // 40.height,
-              // GestureDetector(
-              //   onTap: () {},
-              //   child: Container(
-              //     padding:
-              //         const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              //     width: double.infinity,
-              //     alignment: Alignment.center,
-              //     decoration: BoxDecoration(
-              //       borderRadius: BorderRadius.circular(10),
-              //       border: Border.all(color: AppColors.primaryColor),
-              //     ),
-              //     child: Text(
-              //       AppText.shopNowText,
-              //       style: TextStyle(
-              //         color: AppColors.primaryColor,
-              //         fontSize: 20,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              ListView.builder(
-                  itemCount: 2,
-                  shrinkWrap: true,
-                  physics: AlwaysScrollableScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        CartCardWidget(),
-                        Divider(),
-                      ],
-                    );
-                  }),
-              SizedBox(
-                height: 20.h,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                child: PrimaryTextWidget(
-                  text: 'Promo Code',
-                  fontSize: 15,
-                  fontFamily: AppFonts.robotoBold,
-                ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 10.w),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 55.h,
-                      width: MediaQuery.of(context).size.width / 1.3,
-                      child: TextFieldWidget(
-                        controller: _promocode,
-                        label: 'Enter Code Here',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.w,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        bottomSheet();
-                      },
-                      child: Container(
-                        height: 50.h,
-                        width: 45.w,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5.r),
-                            color: Colors.black),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Divider(),
-              SizedBox(
-                height: 20.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10.w),
+              child: Row(
                 children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: PrimaryTextWidget(
-                      text: 'Sub total',
-                      fontSize: 14,
-                      fontFamily: AppFonts.robotoLight,
+                  Container(
+                    height: 55.h,
+                    width: MediaQuery.of(context).size.width / 1.3,
+                    child: TextFieldWidget(
+                      controller: _promocode,
+                      label: 'Enter Code Here',
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: PrimaryTextWidget(
-                      text: '\$300.00',
-                      fontSize: 14,
-                      fontFamily: AppFonts.robotoLight,
-                    ),
+                  SizedBox(
+                    width: 10.w,
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: SecondaryTextWidget(
-                      text: 'Discount',
-                      fontSize: 14,
-                      fontFamily: AppFonts.robotoLight,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: PrimaryTextWidget(
-                      text: '\$0.00',
-                      fontSize: 14,
-                      fontFamily: AppFonts.robotoLight,
-                      fontColor: Colors.green,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: SecondaryTextWidget(
-                      text: 'Delivery',
-                      fontSize: 14,
-                      fontFamily: AppFonts.robotoLight,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: PrimaryTextWidget(
-                      text: '+\$15.00',
-                      fontSize: 14,
-                      fontFamily: AppFonts.robotoLight,
-                      fontColor: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Divider(),
-              SizedBox(
-                height: 20.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        PrimaryTextWidget(
-                          text: 'Total',
-                          fontSize: 14,
-                          fontFamily: AppFonts.robotoLight,
-                        ),
-                        SizedBox(
-                          height: 5.h,
-                        ),
-                        PrimaryTextWidget(
-                          text: '\$315.00',
-                          fontSize: 14,
-                          fontFamily: AppFonts.robotoLight,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  GestureDetector(
+                    onTap: () {
+                      bottomSheet();
+                    },
                     child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5.r),
-                            border: Border.all(color: AppColors.primaryColor)),
-                        height: 40.h,
-                        child: Padding(
-                          padding: EdgeInsets.all(8.w),
-                          child: PrimaryTextWidget(
-                            text: 'Proceed To Checkout',
-                            fontColor: AppColors.primaryColor,
-                          ),
-                        )),
+                      height: 50.h,
+                      width: 45.w,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.r),
+                          color: Colors.black),
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+            Divider(),
+            SizedBox(
+              height: 20.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: PrimaryTextWidget(
+                    text: 'Sub total',
+                    fontSize: 14,
+                    fontFamily: AppFonts.robotoLight,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: PrimaryTextWidget(
+                    text: '\$300.00',
+                    fontSize: 14,
+                    fontFamily: AppFonts.robotoLight,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: SecondaryTextWidget(
+                    text: 'Discount',
+                    fontSize: 14,
+                    fontFamily: AppFonts.robotoLight,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: PrimaryTextWidget(
+                    text: '\$0.00',
+                    fontSize: 14,
+                    fontFamily: AppFonts.robotoLight,
+                    fontColor: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: SecondaryTextWidget(
+                    text: 'Delivery',
+                    fontSize: 14,
+                    fontFamily: AppFonts.robotoLight,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: PrimaryTextWidget(
+                    text: '+\$15.00',
+                    fontSize: 14,
+                    fontFamily: AppFonts.robotoLight,
+                    fontColor: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Divider(),
+            SizedBox(
+              height: 10.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      PrimaryTextWidget(
+                        text: 'Total',
+                        fontSize: 14,
+                        fontFamily: AppFonts.robotoLight,
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      PrimaryTextWidget(
+                        text: '\$315.00',
+                        fontSize: 14,
+                        fontFamily: AppFonts.robotoLight,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.w, right: 20.w),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.r),
+                          border: Border.all(color: AppColors.primaryColor)),
+                      height: 40.h,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.w),
+                        child: PrimaryTextWidget(
+                          text: 'Proceed To Checkout',
+                          fontColor: AppColors.primaryColor,
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
